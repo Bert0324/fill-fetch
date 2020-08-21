6dea17e69c24d48ce748aaf5dd86bc569c7fa909
“yuchen.huang” <yuchen.huang@wz-inc.com>
2020-08-21 16:38:46 +0800

    feat: types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,7 @@
+/**
+ * request configuration types
+ * @default undefined
+ */
 export interface IConfig extends RequestInit {
     baseURL?: string;
     timeout?: number;
@@ -9,10 +13,22 @@ export type responseInterceptor = <T = any>(response: T) => T | Promise<T>;
 export type responseErrorCatch = (err: any) => any;
 
 export interface IInterceptor {
+    /**
+     * request interceptors
+     */
     request: requestInterceptor[] & {
+        /**
+         * append callback to request interceptor
+         */
         use: (interceptor: requestInterceptor) => void;
     };
+    /**
+     * response interceptors
+     */
     response: responseInterceptor[] & {
+        /**
+         * append callback to response interceptor
+         */
         use: (interceptor: responseInterceptor, catcher?: responseErrorCatch) => void;
     };
     errorHandlers: responseErrorCatch[];
@@ -20,12 +36,27 @@ export interface IInterceptor {
 
 export type Fetch = typeof fetch;
 export interface IFilledFetch extends Fetch {
+    /**
+     * quick get function
+     */
     get<T = any>(path: string, params?: any, config?: IConfig): Promise<T>;
+    /**
+     * quick post function
+     */
     post<T = any>(path: string, data?: any, config?: IConfig): Promise<T>;
+    /**
+     * interceptors
+     */
     interceptors: IInterceptor;
+    /**
+     * request configuration
+     */
     config: IConfig;
 }
 
 declare module 'fill-fetch' {
+    /**
+     * fill native `window.fetch`
+     */
     function fill(): IFilledFetch;
 }
